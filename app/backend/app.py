76c6c8ba8e631ce3fb5ec45baba41a2c8f37e9a7
Pascal <pascal@dzikus.net>
2026-03-16 12:00:51 +0100

    Add RAG functionality

--- app/backend/app.py
+++ app/backend/app.py
@@ -7,6 +7,7 @@ from azure.core.credentials import AzureKeyCredential
 from azure.identity import AzureDeveloperCliCredential, DefaultAzureCredential
 from dotenv import load_dotenv
 
+from ragtools import attach_rag_tools
 from rtmt import RTMiddleTier
 
 logging.basicConfig(level=logging.INFO)
@@ -18,9 +19,10 @@ async def create_app():
         load_dotenv()
 
     llm_key = os.environ.get("AZURE_OPENAI_API_KEY")
+    search_key = os.environ.get("AZURE_SEARCH_API_KEY")
 
     credential = None
-    if not llm_key:
+    if not llm_key or not search_key:
         if tenant_id := os.environ.get("AZURE_TENANT_ID"):
             logger.info("Using AzureDeveloperCliCredential with tenant_id %s", tenant_id)
             credential = AzureDeveloperCliCredential(tenant_id=tenant_id, process_timeout=60)
@@ -28,6 +30,7 @@ async def create_app():
             logger.info("Using DefaultAzureCredential")
             credential = DefaultAzureCredential()
     llm_credential = AzureKeyCredential(llm_key) if llm_key else credential
+    search_credential = AzureKeyCredential(search_key) if search_key else credential
     
     app = web.Application()
 
@@ -38,9 +41,26 @@ async def create_app():
         voice_choice=os.environ.get("AZURE_OPENAI_REALTIME_VOICE_CHOICE") or "alloy"
         )
     rtmt.system_message = """
-        You are a helpful assistant. Answer questions directly and concisely.
-        The user is listening to answers with audio, so it's important that answers are as short as possible, a single sentence if at all possible.
+        You are a helpful assistant. Only answer questions based on information you searched in the knowledge base, accessible with the 'search' tool.
+        The user is listening to answers with audio, so it's *super* important that answers are as short as possible, a single sentence if at all possible.
+        Never read file names or source names or keys out loud.
+        Always use the following step-by-step instructions to respond:
+        1. Always use the 'search' tool to check the knowledge base before answering a question.
+        2. Always use the 'report_grounding' tool to report the source of information from the knowledge base.
+        3. Produce an answer that's as short as possible. If the answer isn't in the knowledge base, say you don't know.
     """.strip()
+
+    attach_rag_tools(rtmt,
+        credentials=search_credential,
+        search_endpoint=os.environ["AZURE_SEARCH_ENDPOINT"],
+        search_index=os.environ["AZURE_SEARCH_INDEX"],
+        semantic_configuration=os.environ.get("AZURE_SEARCH_SEMANTIC_CONFIGURATION") or None,
+        identifier_field=os.environ.get("AZURE_SEARCH_IDENTIFIER_FIELD") or "chunk_id",
+        content_field=os.environ.get("AZURE_SEARCH_CONTENT_FIELD") or "chunk",
+        embedding_field=os.environ.get("AZURE_SEARCH_EMBEDDING_FIELD") or "text_vector",
+        title_field=os.environ.get("AZURE_SEARCH_TITLE_FIELD") or "title",
+        use_vector_query=(os.getenv("AZURE_SEARCH_USE_VECTOR_QUERY", "true") == "true")
+        )
 
     rtmt.attach_to_app(app, "/realtime")
 
